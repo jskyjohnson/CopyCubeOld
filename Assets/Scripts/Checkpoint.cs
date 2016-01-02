46b96947e44a97ab35a168c3dcd7e802ef44672d
AndyY <ayan9005@bvsd.org>
2016-01-02 12:26:58 -0700

    Updates

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -11,15 +11,16 @@ public class Checkpoint : MonoBehaviour {
 			Player.respawnDirection = Player.direction;
 			Player.respawnLocation = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
 			Player.respawnInverted = coll.gameObject.GetComponent<Player>().inverted;
+			Destroy(GetComponent<Collider>());
 			StartCoroutine(shrink());
 		}
 	}
 
 	IEnumerator shrink() {
 		while(transform.localScale.y > 0f) {
-			transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - (Time.deltaTime * 12f), transform.localScale.z);
+			transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - (Time.deltaTime * 14f), transform.localScale.z);
 			yield return 0;
 		}
-		gameObject.SetActive(false);
+		Destroy(GetComponent<SkinnedMeshRenderer>());
 	}
 }
